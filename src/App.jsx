ab83030dedce03538ef7bbe1031ce311122e21df
Tor <Torskaa@gmail.com>
2025-04-01 15:24:12 -0400

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 import "./App.css";
+import "./style.css";
 import { puppyList } from "./data.js";
 import { useState } from "react";
+
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log(puppies);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   return (
     <div>
       {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>;
+        return (
+          <p
+            onClick={() => {
+              setFeatPupId(puppy.id);
+            }}
+            key={puppy.id}
+          >
+            {puppy.name}
+          </p>
+        );
       })}
+      {featPupId && (
+        <div className="PUPS">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
